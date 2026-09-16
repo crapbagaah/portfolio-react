a0235eb56b163ee9396b5e5dc613cec0d6d15375
Saheli Sahoo <90127903+crapbagaah@users.noreply.github.com>
2026-09-16 22:06:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import { useState, useEffect } from "react";
+
 import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
+import Experience from "./components/experience";
 import Skills from "./components/skills";
 import Work from "./components/work";
 import Contact from "./components/contact";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   return (
     <div>
-      <Navbar />
+      <Navbar
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
+
       <Home />
       <About />
+      <Experience />
       <Skills />
       <Work />
       <Contact />
-      </div>
+    </div>
   );
 }
 
